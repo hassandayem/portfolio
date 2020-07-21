b01e4382d685cf0bcb6a90b18d4e496cb5f44bfe
Hassan Dayem <hassandayem91@gmail.com>
2020-07-21 19:41:41 +0300

    Updates

---
+++ precache-manifest.e1e372694955a5151c0299ac52bfe7b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28df6588be83c5b6b74a8670da1543fd",
+    "url": "/hassandayem/my-portfolio/index.html"
+  },
+  {
+    "revision": "ebd42d8f9371f1cd6bd9",
+    "url": "/hassandayem/my-portfolio/static/css/main.23ba4ee1.chunk.css"
+  },
+  {
+    "revision": "617de30d8753331c79c8",
+    "url": "/hassandayem/my-portfolio/static/js/2.c1dd7635.chunk.js"
+  },
+  {
+    "revision": "ebd42d8f9371f1cd6bd9",
+    "url": "/hassandayem/my-portfolio/static/js/main.603b1d67.chunk.js"
+  },
+  {
+    "revision": "f32c3e2a9462cd470449",
+    "url": "/hassandayem/my-portfolio/static/js/runtime~main.d081dda9.js"
+  }
+]);
